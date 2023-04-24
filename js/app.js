c67e6ab42bb8f7cb5bcc5f9dea8903f0085124d6
basit333 <shahbasit741@gmail.com>
2023-04-25 01:58:48 +0500

    made mobile menu

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
+// Footer Input Variables
 // Get the input and label elements
-const emailInput = document.getElementById("email-input");
-const emailLabel = document.getElementById("email-label");
+const footerEmailInput = document.getElementById("email-input");
+const footerEmailLabel = document.getElementById("email-label");
 
+// Footer Input
 // Add an event listener to the input field
-emailInput.addEventListener("input", function () {
+footerEmailInput.addEventListener("input", function () {
   // Check if the input field has a value
-  if (emailInput.value.length > 0) {
+  if (footerEmailInput.value.length > 0) {
     // Hide the label
-    emailLabel.style.display = "none";
+    footerEmailLabel.style.display = "none";
   } else {
     // Show the label
-    emailLabel.style.display = "block";
+    footerEmailLabel.style.display = "block";
   }
+});
+
+// Mobile Navbar
+// Mobile Navbar Variables
+const navOpenBtn = document.querySelector(".nav__menu--btn-open");
+const navCloseBtn = document.querySelector(".nav__menu--btn-close");
+const navMobileMenu = document.querySelector(".mobile__nav--links");
+
+navOpenBtn.addEventListener("click", function () {
+  navMobileMenu.classList.add("mobile__nav--links-open");
+});
+
+navCloseBtn.addEventListener("click", function () {
+  navMobileMenu.classList.remove("mobile__nav--links-open");
 });
